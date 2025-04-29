9600e7ea0113013cf4f2007eee2f856db91947ce
Abdaisy conceicao <abdaisyconceicao@epfundao.edu.pt>
2025-04-29 16:29:32 +0100

    vit config

--- meu-portfolio/vite.config.js
+++ meu-portfolio/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ✅ isso aqui é o importante
   plugins: [react()],
-  base: '/meu-portfolio/', // Use o nome da pasta ou subdiretório onde o portfólio estará hospedado
 })
